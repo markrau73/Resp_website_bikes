ac8b78d295aa0df5489350688466eb7509c60120
markrau73 <m.krauzowicz@wp.pl>
2017-08-22 21:36:01 +0200

    Kolejne sekcje strony wraz z animacją

--- js/app.js
+++ js/app.js
@@ -1,23 +1,44 @@
+// section menu - drop down menu;
+
+let visibleList = document.querySelectorAll('.visible-li');
+let hiddenList = document.querySelectorAll('.hidden-menu');
+
+
+visibleList.forEach( (elem) => {
+  elem.addEventListener('mouseover', () => {
+    let hiddenList = elem.querySelector('.hidden-menu');
+    hiddenList.style.display = "inline-block";
+  });
+  elem.addEventListener('mouseout', () => {
+    setTimeout( () => {
+        let hiddenList = elem.querySelector('.hidden-menu');
+        hiddenList.style.display = "none";
+      }, 3000);
+    });
+
+});
+
+// section class="sixth" - slider;
 
 let slides = document.querySelectorAll('.slide');
 let dots = document.querySelectorAll('.control-dot');
 
-for(var j=0; j<slides.length; j++){
+for(let j=0; j<slides.length; j++){
   slides[j].classList.add('invisible');
 }
 slides[0].classList.remove('invisible');
 
-for(var i = 0; i < dots.length; i++){
+for(let i = 0; i < dots.length; i++){
   dots[i].addEventListener('click', (e) => {
       let id = e.target.dataset.number;
-    for(var j=0; j<slides.length; j++){
+    for(let j=0; j<slides.length; j++){
       slides[j].classList.add('invisible');
     }
     slides[id - 1].classList.remove('invisible');
   })
 }
 
-for(var i = 0; i < dots.length; i++){
+for(let i = 0; i < dots.length; i++){
   dots[i].classList.remove('active');
 }
   dots[0].classList.add('active');
@@ -39,3 +60,109 @@ for(var i = 0; i < dots.length; i++){
     dots[0].classList.remove('active');
     dots[1].classList.remove('active');
   } )
+
+
+// white logos appearing with delay during scroll
+
+  window.sr = ScrollReveal();
+
+  sr.reveal('.left_pic, .vintage_logo, .bicycles_logo', {delay: 600});
+
+  // section class = "seventh" pictures animation-name
+
+  let animated = document.querySelectorAll('.animated');
+
+  for (let i = 0; i < animated.length; i++){
+    animated[i].addEventListener('mouseover', (e) => {
+      e.target.style.background = "#5ec29d";
+      let img = animated[i].querySelector('.img_animated');
+      img.style.visibility = "visible";
+      img.style.background = "#5ec29d";
+      let text = animated[i].querySelector('.expand');
+      text.style.visibility = "visible";
+      text.style.background = "#5ec29d";
+    });
+  }
+
+  for (let i = 0; i < animated.length; i++){
+    animated[0].addEventListener('mouseleave', (e) => {
+      e.target.style = {
+        background: 'url("images/l1.jpg") center center no-repeat',
+        size: 'cover'
+      };
+      let img = animated[0].querySelector('.img_animated');
+      img.style.visibility = "hidden";
+      let text = animated[0].querySelector('.expand');
+      text.style.visibility = "hidden";
+    });
+    animated[1].addEventListener('mouseleave', (e) => {
+      e.target.style = {
+        background: 'url("images/l2.jpg") center center no-repeat',
+        size: 'cover'
+      };
+      let img = animated[1].querySelector('.img_animated');
+      img.style.visibility = "hidden";
+      let text = animated[1].querySelector('.expand');
+      text.style.visibility = "hidden";
+    });
+    animated[2].addEventListener('mouseleave', (e) => {
+      e.target.style = {
+        background: 'url("images/l3.jpg") center center no-repeat',
+        size: 'cover'
+      };
+      let img = animated[2].querySelector('.img_animated');
+      img.style.visibility = "hidden";
+      let text = animated[2].querySelector('.expand');
+      text.style.visibility = "hidden";
+    });
+    animated[3].addEventListener('mouseleave', (e) => {
+      e.target.style = {
+        background: 'url("images/l4.jpg") center center no-repeat',
+        size: 'cover'
+      };
+      let img = animated[3].querySelector('.img_animated');
+      img.style.visibility = "hidden";
+      let text = animated[3].querySelector('.expand');
+      text.style.visibility = "hidden";
+    });
+    animated[4].addEventListener('mouseleave', (e) => {
+      e.target.style = {
+        background: 'url("images/l5.jpg") center center no-repeat',
+        size: 'cover'
+      };
+      let img = animated[4].querySelector('.img_animated');
+      img.style.visibility = "hidden";
+      let text = animated[4].querySelector('.expand');
+      text.style.visibility = "hidden";
+    });
+    animated[5].addEventListener('mouseleave', (e) => {
+      e.target.style = {
+        background: 'url("images/l6.jpg") center center no-repeat',
+        size: 'cover'
+      };
+      let img = animated[5].querySelector('.img_animated');
+      img.style.visibility = "hidden";
+      let text = animated[5].querySelector('.expand');
+      text.style.visibility = "hidden";
+    });
+    animated[6].addEventListener('mouseleave', (e) => {
+      e.target.style = {
+        background: 'url("images/l7.jpg") center center no-repeat',
+        size: 'cover'
+      };
+      let img = animated[6].querySelector('.img_animated');
+      img.style.visibility = "hidden";
+      let text = animated[6].querySelector('.expand');
+      text.style.visibility = "hidden";
+    });
+    animated[7].addEventListener('mouseleave', (e) => {
+      e.target.style = {
+        background: 'url("images/l8.jpg") center center no-repeat',
+        size: 'cover'
+      };
+      let img = animated[7].querySelector('.img_animated');
+      img.style.visibility = "hidden";
+      let text = animated[7].querySelector('.expand');
+      text.style.visibility = "hidden";
+    });
+  }
